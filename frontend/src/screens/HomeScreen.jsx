ae467b41dbcfa4c5244a2f9beb9a0e8137f6deab
Aby Babu <abyStriveschool@gmail.com>
2021-03-12 18:26:57 +0100

    wishlist

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react'
 import {Row, Col} from 'react-bootstrap'
 import {useDispatch, useSelector} from 'react-redux'
 import {listProducts} from '../actions/productActions'
+import {addToWishList} from '../actions/wishListActions'
 import Product from "../components/Product"
 import Loader from "../components/Loader";
 import Paginate from "../components/Paginate";
@@ -10,13 +11,15 @@ import Meta from '../components/Meta'
 import Message from "../components/Message";
 import {Link} from "react-router-dom";
 
-const HomeScreen = ({match, history}) => {
+const HomeScreen = ({match, history, location}) => {
     const keyword = match.params.keyword
     const pageNumber = match.params.pageNumber || 1
 
     const dispatch = useDispatch()
     const productList = useSelector(state => state.productList)
     const {loading, error, products, page, pages} = productList
+    const productId = match.params.id
+    const qty = location.search ? Number(location.search.split('=')[1]) : 1
 
     useEffect(() => {
         dispatch(listProducts(keyword, pageNumber))

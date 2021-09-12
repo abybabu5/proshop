b912d634d76b278d047ea88c823ad6d8817ea3c1
abybabu5@gmail.com <Qinston123!>
2021-09-12 16:10:14 +0200

    screen fixes

--- frontend/src/screens/ProductListScreen.jsx
+++ frontend/src/screens/ProductListScreen.jsx
@@ -1,121 +1,144 @@
-import React, {useEffect} from 'react';
-import {useDispatch, useSelector} from "react-redux";
-import {LinkContainer} from "react-router-bootstrap";
-import {listProducts, deleteProduct, createProduct } from "../actions/productActions";
-import {Table, Button, Row, Col} from "react-bootstrap";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { LinkContainer } from "react-router-bootstrap";
+import {
+  listProducts,
+  deleteProduct,
+  createProduct,
+} from "../actions/productActions";
+import { Table, Button, Row, Col } from "react-bootstrap";
 import Loader from "../loaders/Loader";
 import Message from "../components/Message";
 import Paginate from "../components/Paginate";
-import {PRODUCT_CREATE_RESET} from "../constants/productConstants";
+import { PRODUCT_CREATE_RESET } from "../constants/productConstants";
 
+const ProductListScreen = ({ history, match }) => {
+  const pageNumber = match.params.pageNumber || 1;
 
-const ProductListScreen = ({history, match}) => {
-    const pageNumber = match.params.pageNumber || 1
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products, page, pages } = productList;
 
-    const productList = useSelector(state => state.productList)
-    const {loading, error, products, page, pages} = productList
+  const productDelete = useSelector((state) => state.productDelete);
+  const {
+    loading: loadingDelete,
+    error: errorDelete,
+    success: successDelete,
+  } = productDelete;
 
-    const productDelete = useSelector(state => state.productDelete)
-    const {loading: loadingDelete, error: errorDelete, success: successDelete} = productDelete
+  const productCreate = useSelector((state) => state.productCreate);
+  const {
+    loading: loadingCreate,
+    error: errorCreate,
+    success: successCreate,
+    product: createdProduct,
+  } = productCreate;
 
-    const productCreate = useSelector(state => state.productCreate)
-    const {loading: loadingCreate, error: errorCreate, success: successCreate, product: createdProduct} = productCreate
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
 
-    const userLogin = useSelector(state => state.userLogin)
-    const {userInfo} = userLogin
-
-
-    useEffect(() => {
-        dispatch({type: PRODUCT_CREATE_RESET})
-        if (!userInfo.isAdmin) {
-            history.push('/login')
-        }
-        if (successCreate) {
-            history.push(`/admin/product/${createdProduct._id}/edit`)
-        } else {
-            dispatch(listProducts('', pageNumber))
-        }
-    }, [
-        dispatch,
-        history,
-        userInfo,
-        successDelete,
-        successCreate,
-        createdProduct,
-        pageNumber
-    ])
-
-
-    const deleteHandler = (id) => {
-        if (window.confirm('Are you sure?')) {
-            dispatch(deleteProduct(id))
-        }
+  useEffect(() => {
+    dispatch({ type: PRODUCT_CREATE_RESET });
+    if (!userInfo.isAdmin) {
+      history.push("/login");
     }
-    const createProductHandler = () => {
-        dispatch(createProduct())
+    if (successCreate) {
+      history.push(`/admin/product/${createdProduct._id}/edit`);
+    } else {
+      dispatch(listProducts("", pageNumber));
     }
+  }, [
+    dispatch,
+    history,
+    userInfo,
+    successDelete,
+    successCreate,
+    createdProduct,
+    pageNumber,
+  ]);
 
-    return (
+  const deleteHandler = (id) => {
+    if (window.confirm("Are you sure?")) {
+      dispatch(deleteProduct(id));
+    }
+  };
+  const createProductHandler = () => {
+    dispatch(createProduct());
+  };
+
+  return (
+    <>
+      <Row className="align-items-center">
+        <Col>
+          <h1>Products</h1>
+        </Col>
+        <Col className="text-right">
+          <Button className="my-3" onClick={createProductHandler}>
+            <i className="fas fa-plus" /> <span>Create Product</span>
+          </Button>
+        </Col>
+      </Row>
+      {loadingDelete && <Loader />}
+      {errorDelete && <Message variant="danger">{errorDelete}</Message>}
+      {loadingCreate && <Loader />}
+      {errorCreate && <Message variant="danger">{errorCreate}</Message>}
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
         <>
-            <Row className='align-items-center'>
-                <Col><h1>Products</h1></Col>
-                <Col className='text-right'>
-                    <Button className='my-3' onClick={createProductHandler}>
-                        <i className='fas fa-plus'/> <span>Create Product</span>
-                    </Button>
-                </Col>
-            </Row>
-            {loadingDelete && <Loader/>}
-            {errorDelete && <Message variant='danger'>{errorDelete}</Message>}
-            {loadingCreate && <Loader/>}
-            {errorCreate && <Message variant='danger'>{errorCreate}</Message>}
-            {loading ? <Loader/>
-                : error ? <Message variant='danger'>{error}</Message>
-                    : ( <>
-                        <Table striped bordered hover responsive className='table-sm'>
-                            <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>NAME</th>
-                                <th>PRICE</th>
-                                <th>CATEGORY</th>
-                                <th>BRAND</th>
-                                <th></th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            {products.map(product => (
-                                <tr key={product._id}>
-                                    <td>{product._id}</td>
-                                    <td>{product.name}</td>
-                                    <td>${product.price}</td>
-                                    <td>{product.category}</td>
-                                    <td>{product.brand}</td>
-                                    <td>
-                                        <LinkContainer to={`/admin/product/${product._id}/edit`}>
-                                            <Button variant='light' className='btn-sm'>
-                                                <i className={'fas fa-edit'}/>
-                                            </Button>
-                                        </LinkContainer>
-                                        <Button variant='danger' className='btn-sm'
-                                                onClick={() => deleteHandler(product._id)}>
-                                            <i className='fas fa-trash'></i>
-                                        </Button>
-                                    </td>
-                                </tr>
-                            ))
-                            }
-                            </tbody>
-                        </Table>
-                            <Paginate pages={pages} page={page} isAdmin={true}/>
-                        </>
-                    )}
-
+          <Table striped bordered hover responsive className="table-sm">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>NAME</th>
+                <th>PRICE</th>
+                <th>CATEGORY</th>
+                <th>BRAND</th>
+                <th>ACTIONS</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product) => (
+                <tr key={product._id}>
+                  <td>{product._id}</td>
+                  <td>{product.name}</td>
+                  <td>${product.price}</td>
+                  <td>{product.category}</td>
+                  <td>{product.brand}</td>
+                  <td>
+                    <div className="row">
+                      <div className="col-6 m-auto">
+                        <LinkContainer
+                          to={`/admin/product/${product._id}/edit`}
+                        >
+                          <Button variant="light" className="btn-sm">
+                            <i className={"fas fa-edit"} />
+                          </Button>
+                        </LinkContainer>
+                      </div>
+                      <div className="col-6 mb-2">
+                        <Button
+                          variant="danger"
+                          className="btn-sm"
+                          onClick={() => deleteHandler(product._id)}
+                        >
+                          <i className="fas fa-trash"></i>
+                        </Button>
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+          <Paginate pages={pages} page={page} isAdmin={true} />
         </>
-    );
-
-}
+      )}
+    </>
+  );
+};
 
 export default ProductListScreen;
--- frontend/src/screens/UserListScreen.jsx
+++ frontend/src/screens/UserListScreen.jsx
@@ -1,83 +1,99 @@
-import React, {useEffect} from 'react';
-import {useDispatch, useSelector} from "react-redux";
-import {LinkContainer} from "react-router-bootstrap";
-import {listUsers, deleteUser} from "../actions/userActions";
-import {Table, Button} from "react-bootstrap";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { LinkContainer } from "react-router-bootstrap";
+import { listUsers, deleteUser } from "../actions/userActions";
+import { Table, Button } from "react-bootstrap";
 import Loader from "../loaders/Loader";
 import Message from "../components/Message";
 
+const UserListScreen = ({ history }) => {
+  const dispatch = useDispatch();
 
-const UserListScreen = ({history}) => {
-    const dispatch = useDispatch()
+  const userList = useSelector((state) => state.userList);
+  const { loading, error, users } = userList;
 
-    const userList = useSelector(state => state.userList)
-    const {loading, error, users} = userList
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
 
-    const userLogin = useSelector(state => state.userLogin)
-    const {userInfo} = userLogin
+  const userDelete = useSelector((state) => state.userDelete);
+  const { success: successDelete } = userDelete;
 
-    const userDelete = useSelector(state => state.userDelete)
-    const {success: successDelete} = userDelete
-
-    useEffect(() => {
-        if (userInfo && userInfo.isAdmin) {
-            dispatch(listUsers())
-        } else {
-            history.push('/login')
-        }
-    }, [dispatch, history, successDelete, userInfo])
-
-
-    const deleteHandler = (id) => {
-        if(window.confirm('Are you sure?')) {
-            dispatch(deleteUser(id))
-        }
+  useEffect(() => {
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listUsers());
+    } else {
+      history.push("/login");
     }
+  }, [dispatch, history, successDelete, userInfo]);
 
-    return (
-        <>
-            <h1>Users</h1>
-            {loading ? <Loader/> : error ? <Message variant='danger'>{error}</Message> : (
-                <Table striped bordered hover responsive className='table-sm'>
-                    <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>NAME</th>
-                        <th>SURNAME</th>
-                        <th>EMAIL</th>
-                        <th>ADMIN</th>
-                        <th></th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {users.map(user => (
-                        <tr key={user._id}>
-                            <td>{user._id}</td>
-                            <td>{user.name}</td>
-                            <td>{user.surname}</td>
-                            <td><a href={`mailto:${user.email}`}>{user.email}</a></td>
-                            <td>{user.isAdmin ? (<i className='fas fa-check' style={{color: 'green'}}></i>)
-                                : (<i className='fas fa-times' style={{color: 'red'}}></i>)}</td>
-                            <td>
-                                <LinkContainer to={`/admin/user/${user._id}/edit`}>
-                                    <Button variant='light' className='btn-sm'>
-                                        <i className={'fas fa-edit'}></i>
-                                    </Button>
-                                </LinkContainer>
-                                <Button variant='danger' className='btn-sm' onClick={() => deleteHandler(user._id)}>
-                                    <i className='fas fa-trash'></i>
-                                </Button>
-                            </td>
-                        </tr>
-                    ))
-                    }
-                    </tbody>
-                </Table>
-            )}
+  const deleteHandler = (id) => {
+    if (window.confirm("Are you sure?")) {
+      dispatch(deleteUser(id));
+    }
+  };
 
-        </>
-    );
-
-}
+  return (
+    <>
+      <h1>Users</h1>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Table striped bordered hover responsive className="table-sm">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NAME</th>
+              <th>SURNAME</th>
+              <th>EMAIL</th>
+              <th>ADMIN</th>
+              <th>ACTION</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user._id}>
+                <td>{user._id}</td>
+                <td>{user.name}</td>
+                <td>{user.surname}</td>
+                <td>
+                  <a href={`mailto:${user.email}`}>{user.email}</a>
+                </td>
+                <td>
+                  {user.isAdmin ? (
+                    <i className="fas fa-check" style={{ color: "green" }}></i>
+                  ) : (
+                    <i className="fas fa-times" style={{ color: "red" }}></i>
+                  )}
+                </td>
+                <td>
+                  <div className="d-flex">
+                    <div className="m-auto">
+                      <LinkContainer to={`/admin/user/${user._id}/edit`}>
+                        <Button variant="outline-dark" className="btn-sm">
+                          <i className={"fas fa-edit"}></i>
+                        </Button>
+                      </LinkContainer>
+                    </div>
+                    <div className="mb-2">
+                      <Button
+                        variant="outline-danger"
+                        className="btn-sm"
+                        onClick={() => deleteHandler(user._id)}
+                      >
+                        <i className="fas fa-trash"></i>
+                      </Button>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+    </>
+  );
+};
 
 export default UserListScreen;

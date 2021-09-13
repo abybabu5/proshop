3929ba84ac72758bdeba81d49a149d5e9e7b149a
abybabu5@gmail.com <Qinston123!>
2021-09-13 18:23:01 +0200

    cookie consent added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -73,10 +73,10 @@ const App = () => {
           Cookies are important to the proper functioning of a site. To improve
           your experience, we use cookies to remember log-in details and provide
           secure log-in, collect statistics to optimize site functionality, and
-          deliver content tailored to your interests. Click Agree and Proceed to
-          accept cookies and go directly to the site or click on View Cookie
-          Settings to see detailed descriptions of the types of cookies and
-          choose whether to accept certain cookies while on the site.
+          deliver content tailored to your interests. Click I understand and
+          Proceed to accept cookies and go directly to the site or click on View
+          Cookie Settings to see detailed descriptions of the types of cookies
+          and choose whether to accept certain cookies while on the site.
         </CookieConsent>
       </div>
       <Footer />

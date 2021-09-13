6d0fac982d05739302ed406dc693fc086cff4969
abybabu5@gmail.com <Qinston123!>
2021-09-13 08:28:32 +0200

    cookie consent added

--- backend/server.js
+++ backend/server.js
@@ -1,78 +1,89 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import express from "express";
+import dotenv from "dotenv";
 import path from "path";
-import colors from 'colors'
-import listEndpoints from 'express-list-endpoints'
-import morgan from 'morgan'
-import cors from "cors"
+import colors from "colors";
+import listEndpoints from "express-list-endpoints";
+import morgan from "morgan";
+import cors from "cors";
 import Stripe from "stripe";
-import bodyParser from 'body-parser'
-import connectDB from './config/db.js'
-import {notFound, errorHandler} from "./middleware/errorMiddileware.js";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddileware.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 import wishListRoutes from "./routes/wishListRoutes.js";
-import {verifyEmail} from "./controllers/userController.js";
+import { verifyEmail } from "./controllers/userController.js";
 
+dotenv.config();
+connectDB();
+const secret = process.env.STRIPE_SECRET_KEY;
+export const stripe = new Stripe(secret);
 
-dotenv.config()
-connectDB()
-const secret = process.env.STRIPE_SECRET_KEY
-export const stripe = new Stripe(secret)
-
-const app = express()
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+const app = express();
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
 // app.use(bodyParser.urlencoded({extended: true}))
 // app.use(bodyParser.json)
 
-app.use('/api/products', productRoutes)
-app.use('/api/users', userRoutes)
-app.use('/api/orders', orderRoutes)
-app.use('/api/upload', uploadRoutes)
-app.use('/api/wishlist', wishListRoutes)
-app.get('/verify-email', verifyEmail)
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
+app.use("/api/wishlist", wishListRoutes);
+app.get("/verify-email", verifyEmail);
 
-app.post('/api/payments/create', async (request, response) => {
-    const {totalPrice} = request.body;
-    console.log('Payment request Received BOOM!! for this amount: ', totalPrice, request.body)
-    const paymentIntent = await stripe.paymentIntents.create({
-        amount: totalPrice,
-        currency: "usd"
-    });
-    // OK- Created
-    response.status(201).send({
-        clientSecret: paymentIntent.client_secret,
-    })
-})
+app.post("/api/payments/create", async (request, response) => {
+  const { totalPrice } = request.body;
+  console.log(
+    "Payment request Received BOOM!! for this amount: ",
+    totalPrice,
+    request.body
+  );
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: totalPrice,
+    currency: "usd",
+  });
+  // OK- Created
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
 
-app.get('/api/config/paypal', (req, res) =>
-    res.send(process.env.PAYPAL_CLIENT_ID)
-)
+app.get("/api/config/paypal", (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
 
-const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/build')))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-    app.get('*', (req, res) =>
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    )
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
 } else {
-    app.get('/', (req, res) => {
-        res.send('API is running...')
-    })
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
 }
 
-app.use(notFound)
-app.use(errorHandler)
-console.log(listEndpoints(app))
+app.use(notFound);
+app.use(errorHandler);
+console.log(listEndpoints(app));
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`Sever is running in ${process.env.NODE_ENV} mode on port: ${PORT}`.rainbow.bold))
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(
+    `Sever is running in ${process.env.NODE_ENV} mode on port: ${PORT}`.rainbow
+      .bold
+  )
+);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
-import {Container} from 'react-bootstrap'
-import Header from "./components/Header"
-import Footer from "./components/Footer"
-import HomeScreen from "./screens/HomeScreen"
-import ProductScreen from "./screens/ProductScreen"
-import CartScreen from "./screens/CartScreen"
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ProfileScreen from "./screens/ProfileScreen";
@@ -21,52 +21,67 @@ import OrderListScreen from "./screens/OrderListScreen";
 import WishListScreen from "./screens/WishListScreen";
 import VerifyEmailScreen from "./screens/VerifyEmailScreen";
 import useSticky from "./stickyNav";
+import CookieConsent from "react-cookie-consent";
 
 const App = () => {
-    const { isSticky, element } = useSticky()
-    return (
-        <Router>
-            <Header sticky={isSticky}/>
-            <div className='py-3' ref={element}>
-                <Container>
-                    <Route path='/order/:id' component={OrderScreen}/>
-                    <Route path='/shipping' component={ShippingScreen}/>
-                    <Route path='/payment' component={PaymentMethodScreen}/>
-                    <Route path='/placeorder' component={PlaceOrderScreen}/>
-                    <Route path='/login' component={LoginScreen}/>
-                    <Route path='/register' component={RegisterScreen}/>
-                    <Route path='/verify-email' component={VerifyEmailScreen}/>
-                    <Route path='/profile' component={ProfileScreen}/>
-                    <Route path='/product/:id' component={ProductScreen}/>
-                    <Route path='/wishlist/:id?' component={WishListScreen}/>
-                    <Route path='/cart/:id?' component={CartScreen}/>
-                    <Route path='/admin/userlist' component={UserListScreen}/>
-                    <Route path='/admin/user/:id/edit' component={UserEditScreen}/>
-                    <Route
-                        path='/admin/productlist'
-                        component={ProductListScreen}
-                        exact
-                    />
-                    <Route
-                        path='/admin/productlist/:pageNumber'
-                        component={ProductListScreen}
-                        exact
-                    />
-                    <Route path='/admin/product/:id/edit' component={ProductEditScreen}/>
-                    <Route path='/admin/orderlist' component={OrderListScreen}/>
-                    <Route path='/search/:keyword' component={HomeScreen} exact/>
-                    <Route path='/page/:pageNumber' component={HomeScreen} exact/>
-                    <Route
-                        path='/search/:keyword/page/:pageNumber'
-                        component={HomeScreen}
-                        exact
-                    />
-                    <Route path='/' component={HomeScreen} exact/>
-                </Container>
-            </div>
-            <Footer/>
-        </Router>
-    );
-}
+  const { isSticky, element } = useSticky();
+  return (
+    <Router>
+      <Header sticky={isSticky} />
+      <div className="py-3" ref={element}>
+        <Container>
+          <Route path="/order/:id" component={OrderScreen} />
+          <Route path="/shipping" component={ShippingScreen} />
+          <Route path="/payment" component={PaymentMethodScreen} />
+          <Route path="/placeorder" component={PlaceOrderScreen} />
+          <Route path="/login" component={LoginScreen} />
+          <Route path="/register" component={RegisterScreen} />
+          <Route path="/verify-email" component={VerifyEmailScreen} />
+          <Route path="/profile" component={ProfileScreen} />
+          <Route path="/product/:id" component={ProductScreen} />
+          <Route path="/wishlist/:id?" component={WishListScreen} />
+          <Route path="/cart/:id?" component={CartScreen} />
+          <Route path="/admin/userlist" component={UserListScreen} />
+          <Route path="/admin/user/:id/edit" component={UserEditScreen} />
+          <Route
+            path="/admin/productlist"
+            component={ProductListScreen}
+            exact
+          />
+          <Route
+            path="/admin/productlist/:pageNumber"
+            component={ProductListScreen}
+            exact
+          />
+          <Route path="/admin/product/:id/edit" component={ProductEditScreen} />
+          <Route path="/admin/orderlist" component={OrderListScreen} />
+          <Route path="/search/:keyword" component={HomeScreen} exact />
+          <Route path="/page/:pageNumber" component={HomeScreen} exact />
+          <Route
+            path="/search/:keyword/page/:pageNumber"
+            component={HomeScreen}
+            exact
+          />
+          <Route path="/" component={HomeScreen} exact />
+        </Container>
+        <CookieConsent
+          location="bottom"
+          cookieName="myAwesomeCookieName3"
+          expires={999}
+          overlay
+        >
+          Cookies are important to the proper functioning of a site. To improve
+          your experience, we use cookies to remember log-in details and provide
+          secure log-in, collect statistics to optimize site functionality, and
+          deliver content tailored to your interests. Click Agree and Proceed to
+          accept cookies and go directly to the site or click on View Cookie
+          Settings to see detailed descriptions of the types of cookies and
+          choose whether to accept certain cookies while on the site.
+        </CookieConsent>
+      </div>
+      <Footer />
+    </Router>
+  );
+};
 
 export default App;
